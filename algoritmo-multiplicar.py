7aa05a10d330fdd3e1ed5e75502367fa5f0ffde3
Taina <taina.araldi1@gmail.com>
2026-05-29 23:59:17 -0300

    criado algoritmo de multiplicação

---
+++ algoritmo-multiplicar.py
@@ -0,0 +1,4 @@
+valorUm = int(input("Digite o primeiro valor: "))
+valorDois = int(input("Digite o segundo valor: "))
+resultado = valorUm * valorDois
+print(f"O resultado da multiplicação é: {resultado}")
